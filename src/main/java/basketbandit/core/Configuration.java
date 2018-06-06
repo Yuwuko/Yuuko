d8ef18ee776b8ff76bccd2be2ea95d920f7aa33e
Josh <errorsfound@hotmail.co.jp>
2018-06-06 16:29:49 +0100

    version update (doh!)

--- src/main/java/basketbandit/core/Configuration.java
+++ src/main/java/basketbandit/core/Configuration.java
@@ -6,7 +6,7 @@ package basketbandit.core;
 public class Configuration {
 
     // Bot version.
-    public static final String VERSION = "1.9.3";
+    public static final String VERSION = "1.9.4";
 
     // Bot's ID.
     public static String BOT_ID;
