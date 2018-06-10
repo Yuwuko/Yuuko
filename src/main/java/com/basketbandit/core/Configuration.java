cf182d4d055ea92935a24c6b34228da86701bc2b
Josh <errorsfound@hotmail.co.jp>
2018-06-10 16:38:30 +0100

    updated version

--- src/main/java/com/basketbandit/core/Configuration.java
+++ src/main/java/com/basketbandit/core/Configuration.java
@@ -2,7 +2,7 @@ package com.basketbandit.core;
 
 public class Configuration {
     // Bot version.
-    public static final String VERSION = "1.10.1";
+    public static final String VERSION = "2.0.0";
 
     // Bot's ID.
     public static String BOT_ID;
