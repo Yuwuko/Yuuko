3ead0e08594cda0f97ce7a5e321bfa6e4dc1a82f
Josh <errorsfound@hotmail.co.jp>
2018-09-29 14:41:31 +0100

    converted weather to metric, parsed sunrise, sunset unix strings to readable date.

--- src/main/java/com/basketbandit/core/modules/utility/commands/CommandWeather.java
+++ src/main/java/com/basketbandit/core/modules/utility/commands/CommandWeather.java
@@ -11,6 +11,8 @@ import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
 import java.awt.*;
+import java.time.Instant;
+import java.util.Date;
 
 public class CommandWeather extends Command {
 
@@ -28,7 +30,7 @@ public class CommandWeather extends Command {
             String[] commandParameters = command[1].split("\\s+", 2);
             commandParameters[0] = command[1].replace(" ", "+");
 
-            String json = new JsonBuffer().getString("api.openweathermap.org/data/2.5/weather?q=" +commandParameters[0] + "&APPID=" + Configuration.OPEN_WEATHER_MAP_API);
+            String json = new JsonBuffer().getString("https://api.openweathermap.org/data/2.5/weather?q=" +commandParameters[0] + "&units=metric&APPID=" + Configuration.OPEN_WEATHER_MAP_API);
 
             if(json != null && json.equals("")) {
                 Utils.sendMessage(e,"Sorry " + e.getAuthor().getAsMention() + ", unable to retrieve weather information from " + command[1] + ".");
@@ -42,17 +44,17 @@ public class CommandWeather extends Command {
                     .setTitle("Weather information for: " + weather.getName() + ", " + weather.getSys().getCountry())
                     .setThumbnail("https://openweathermap.org/img/w/" + weather.getWeather().get(0).getIcon() + ".png")
                     .addField("ID", weather.getId() + "", true)
+                    .addField("Visibility", weather.getVisibility() + "m", true)
                     .addField("Latitude", weather.getCoord().getLat() + "", true)
                     .addField("Longitude", weather.getCoord().getLon() + "", true)
-                    .addField("Sunrise", weather.getSys().getSunrise() + "", true)
-                    .addField("Sunset", weather.getSys().getSunset() + "", true)
+                    .addField("Sunrise", Date.from(Instant.ofEpochSecond(weather.getSys().getSunrise())) + "", true)
+                    .addField("Sunset", Date.from(Instant.ofEpochSecond(weather.getSys().getSunset())) + "", true)
                     .addField("Humidity", weather.getMain().getHumidity() + "%", true)
                     .addField("Temperature", weather.getMain().getTemp() + "c", true)
                     .addField("Max Temperature", weather.getMain().getTempMax() + "c", true)
                     .addField("Min Temperature", weather.getMain().getTempMin() + "c", true)
                     .addField("Wind Speed", weather.getWind().getSpeed() + "mph", true)
                     .addField("Wind Angle", weather.getWind().getDeg() + "°", true)
-                    .addField("Visibility", weather.getVisibility() + "", true)
                     .setFooter("Version: " + Configuration.VERSION + ", Data provided by openweathermap.org" , e.getGuild().getMemberById(Configuration.BOT_ID).getUser().getAvatarUrl());
             Utils.sendMessage(e, embed.build());
 
