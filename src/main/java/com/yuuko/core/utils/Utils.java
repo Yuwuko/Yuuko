b542d087e852809efc2ff2220d51da4cb7e4fbf6
Josh <errorsfound@hotmail.co.jp>
2018-12-28 03:25:53 +0000

    updated mute command to stop muted users for speaking

--- src/main/java/com/yuuko/core/utils/Utils.java
+++ src/main/java/com/yuuko/core/utils/Utils.java
@@ -88,6 +88,7 @@ public final class Utils {
     public static Role setupMutedRole(Guild guild) {
         GuildController controller = guild.getController();
         List<TextChannel> channels = guild.getTextChannels();
+        List<VoiceChannel> voiceChannels = guild.getVoiceChannels();
         List<Role> roleList = guild.getRoles();
         Role muted = null;
 
@@ -102,23 +103,29 @@ public final class Utils {
             muted = controller.createRole().setName("Muted").setPermissions(Permission.MESSAGE_READ, Permission.MESSAGE_HISTORY, Permission.VOICE_CONNECT).complete();
 
             for(TextChannel channel: channels) {
-                channel.createPermissionOverride(muted).setDeny(Permission.MESSAGE_WRITE, Permission.MESSAGE_ADD_REACTION, Permission.VOICE_SPEAK, Permission.VOICE_USE_VAD).complete();
+                channel.createPermissionOverride(muted).setDeny(Permission.MESSAGE_WRITE, Permission.MESSAGE_ADD_REACTION).complete();
             }
-        }
 
-        for(TextChannel channel: channels) {
-            PermissionOverride override = null;
+            for(VoiceChannel channel: voiceChannels) {
+                channel.createPermissionOverride(muted).setDeny(Permission.VOICE_SPEAK).complete();
+            }
 
-            for(PermissionOverride channelOverride: channel.getRolePermissionOverrides()) {
-                if(channelOverride.getRole().equals(muted)) {
-                    override = channelOverride;
-                    break;
+
+            for(TextChannel channel: channels) {
+                PermissionOverride override = null;
+
+                for(PermissionOverride channelOverride: channel.getRolePermissionOverrides()) {
+                    if(channelOverride.getRole().equals(muted)) {
+                        override = channelOverride;
+                        break;
+                    }
+                }
+
+                if(override == null) {
+                    channel.createPermissionOverride(muted).setDeny(Permission.MESSAGE_WRITE, Permission.MESSAGE_ADD_REACTION, Permission.VOICE_SPEAK, Permission.VOICE_USE_VAD).complete();
                 }
             }
 
-            if(override == null) {
-                channel.createPermissionOverride(muted).setDeny(Permission.MESSAGE_WRITE, Permission.MESSAGE_ADD_REACTION, Permission.VOICE_SPEAK, Permission.VOICE_USE_VAD).complete();
-            }
         }
 
         return muted;
