a63430ca3a4cec24163e72db2aadd493b863fe7b
Josh <errorsfound@hotmail.co.jp>
2019-01-09 13:50:14 +0000

    added try/catch w/ logging to DatabaseConnection class

--- src/main/java/com/yuuko/core/database/connections/DatabaseConnection.java
+++ src/main/java/com/yuuko/core/database/connections/DatabaseConnection.java
@@ -2,21 +2,29 @@ package com.yuuko.core.database.connections;
 
 import com.yuuko.core.Configuration;
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
+    private static final Logger log = LoggerFactory.getLogger(DatabaseConnection.class);
     private static final BasicDataSource connectionPool = new BasicDataSource();
 
     public DatabaseConnection() {
-        connectionPool.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        connectionPool.setUsername(Configuration.DATABASE_USERNAME);
-        connectionPool.setPassword(Configuration.DATABASE_PASSWORD);
-        connectionPool.setUrl("jdbc:mysql://" + Configuration.DATABASE_IP + "/" + Configuration.DATABASE_NAME + "?useSSL=false");
-        connectionPool.setInitialSize(10);
-        connectionPool.setMaxTotal(100);
-        connectionPool.setMaxIdle(5);
+        try {
+            log.trace("Invoking {}", this.getClass().getName());
+            connectionPool.setDriverClassName("com.mysql.cj.jdbc.Driver");
+            connectionPool.setUsername(Configuration.DATABASE_USERNAME);
+            connectionPool.setPassword(Configuration.DATABASE_PASSWORD);
+            connectionPool.setUrl("jdbc:mysql://" + Configuration.DATABASE_IP + "/" + Configuration.DATABASE_NAME + "?useSSL=false");
+            connectionPool.setInitialSize(10);
+            connectionPool.setMaxTotal(100);
+            connectionPool.setMaxIdle(5);
+        } catch(Exception ex) {
+            log.error("An error occurred while running the {} class, message: {}", this.getClass().getSimpleName(), ex.getMessage(), ex);
+        }
     }
 
     /**
