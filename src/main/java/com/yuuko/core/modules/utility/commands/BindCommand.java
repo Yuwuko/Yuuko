da56017f4f22419a5f53fe02a1aae688e213be4e
Josh <errorsfound@hotmail.co.jp>
2019-01-13 03:10:39 +0000

    upgraded the bind command to stop it being so scuffed, also allowing multiple channels to be bound at once.

--- src/main/java/com/yuuko/core/modules/utility/commands/BindCommand.java
+++ src/main/java/com/yuuko/core/modules/utility/commands/BindCommand.java
@@ -1,12 +1,19 @@
 package com.yuuko.core.modules.utility.commands;
 
+import com.yuuko.core.Cache;
 import com.yuuko.core.database.DatabaseFunctions;
 import com.yuuko.core.modules.Command;
+import com.yuuko.core.modules.Module;
 import com.yuuko.core.modules.utility.UtilityModule;
 import com.yuuko.core.utilities.MessageHandler;
+import com.yuuko.core.utilities.TextUtility;
+import com.yuuko.core.utilities.Utils;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+import java.util.List;
 
 public class BindCommand extends Command {
 
@@ -17,36 +24,52 @@ public class BindCommand extends Command {
     @Override
     public void executeCommand(MessageReceivedEvent e, String[] command) {
         String[] commandParameters = command[1].split("\\s+", 2);
-        String serverId;
-        String channelId;
-        String module;
+        String selectedModule = "";
+
+        boolean present = false;
+        for(Module module : Cache.MODULES) {
+            if(module.getName().equalsIgnoreCase(commandParameters[0])) {
+                selectedModule = module.getName();
+                present = true;
+                break;
+            }
+        }
+
+        if(!present) {
+            EmbedBuilder embed = new EmbedBuilder().setTitle("Invalid Input").setDescription("**" + commandParameters[0] + "** isn't a valid module. A list of valid modules can be found by using the '" + Utils.getServerPrefix(e.getGuild().getId()) + "modules' command.");
+            MessageHandler.sendMessage(e, embed.build());
+            return;
+        }
 
         if(commandParameters.length > 1) {
-            serverId = e.getGuild().getId();
-            channelId = e.getGuild().getTextChannelsByName(commandParameters[1], true).get(0).getId();
-            module = commandParameters[0].toLowerCase();
+            List<TextChannel> channels = e.getMessage().getMentionedChannels();
 
-            final int res = new DatabaseFunctions().toggleBinding(module, channelId, serverId);
-
-            if(res == 0) {
-                EmbedBuilder embed = new EmbedBuilder().setTitle("Successfully bound _" + module + "_ to _" + e.getGuild().getTextChannelsByName(commandParameters[1], true).get(0).getName() + "_.");
+            if(channels.size() < 1) {
+                EmbedBuilder embed = new EmbedBuilder().setTitle("Invalid Input").setDescription("I expected at least **1** \\#tagged channel to bind, but found **0**");
                 MessageHandler.sendMessage(e, embed.build());
-            } else if(res == 1) {
-                EmbedBuilder embed = new EmbedBuilder().setTitle("Successfully removed binding of _" + module + "_ from _" + e.getGuild().getTextChannelsByName(commandParameters[1], true).get(0).getName() + "_.");
-                MessageHandler.sendMessage(e, embed.build());
+                return;
             }
-        } else {
-            serverId = e.getGuild().getId();
-            channelId = e.getTextChannel().getId();
-            module = commandParameters[0];
 
-            final int res = new DatabaseFunctions().toggleBinding(module, channelId, serverId);
+            StringBuilder boundChannels = new StringBuilder();
+            for(TextChannel channel: channels) {
+                new DatabaseFunctions().toggleBinding(selectedModule, channel.getId(), e.getGuild().getId());
+                boundChannels.append(channel.getName()).append(", ");
+            }
+            TextUtility.removeLastOccurrence(boundChannels, ", ");
+
+            EmbedBuilder embed = new EmbedBuilder().setTitle("Successfully toggled **" + selectedModule + "** on **" + boundChannels.toString() + "**.");
+            MessageHandler.sendMessage(e, embed.build());
+        } else {
+            String channelId = e.getTextChannel().getId();
+            selectedModule = commandParameters[0];
+
+            final int res = new DatabaseFunctions().toggleBinding(selectedModule, channelId, e.getGuild().getId());
 
             if(res == 0) {
-                EmbedBuilder embed = new EmbedBuilder().setTitle("Successfully bound _" + module + "_ to _" + e.getTextChannel().getName() + "_.");
+                EmbedBuilder embed = new EmbedBuilder().setTitle("Successfully bound **" + selectedModule + "** to **" + e.getTextChannel().getName() + "**.");
                 MessageHandler.sendMessage(e, embed.build());
             } else if(res == 1) {
-                EmbedBuilder embed = new EmbedBuilder().setTitle("Successfully removed binding of _" + module + "_ from _" + e.getTextChannel().getName() + "_.");
+                EmbedBuilder embed = new EmbedBuilder().setTitle("Successfully removed binding of **" + selectedModule + "** from **" + e.getTextChannel().getName() + "**.");
                 MessageHandler.sendMessage(e, embed.build());
             }
         }
