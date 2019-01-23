1e1501d2e55bd4867d807103f1c3e2c03742318a
Josh <errorsfound@hotmail.co.jp>
2019-01-23 10:08:43 +0000

    added lavalink node information to the shards command

--- src/main/java/com/yuuko/core/commands/core/commands/ShardsCommand.java
+++ src/main/java/com/yuuko/core/commands/core/commands/ShardsCommand.java
@@ -5,6 +5,7 @@ import com.yuuko.core.commands.Command;
 import com.yuuko.core.commands.core.CoreModule;
 import com.yuuko.core.utilities.MessageHandler;
 import com.yuuko.core.utilities.TextUtility;
+import lavalink.client.io.LavalinkSocket;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.entities.Guild;
@@ -36,9 +37,23 @@ public class ShardsCommand extends Command {
         }
         TextUtility.removeLastOccurrence(shards, "\n\n");
 
+        StringBuilder nodes = new StringBuilder();
+        for(LavalinkSocket socket : Cache.LAVALINK.getLavalink().getNodes()) {
+            nodes.append("**Yuuko-").append(socket.getName())
+                    .append("** - ").append(TextUtility.getTimestamp(socket.getStats().getUptime()))
+                    .append("\n").append("System Load: ").append((Math.round(socket.getStats().getSystemLoad()*100))/100)
+                    .append("\n").append("CPU Cores: ").append(socket.getStats().getCpuCores())
+                    .append("\n").append("Memory Used: ").append(socket.getStats().getMemUsed()/1000000).append("MB")
+                    .append("\n").append("Players: ").append(socket.getStats().getPlayers())
+                    .append("\n").append("Playing: ").append(socket.getStats().getPlayingPlayers())
+                    .append("\n\n");
+        }
+        TextUtility.removeLastOccurrence(nodes, "\n\n");
+
         EmbedBuilder shardEmbed = new EmbedBuilder()
                 .setAuthor(Cache.BOT.getName() + "#" + Cache.BOT.getDiscriminator() + " - Shards", null, Cache.BOT.getAvatarUrl())
-                .setDescription(shards.toString());
+                .addField("Yuuko", shards.toString(), false)
+                .addField("Lavalink", nodes.toString(), false);
         MessageHandler.sendMessage(e, shardEmbed.build());
     }
 }
