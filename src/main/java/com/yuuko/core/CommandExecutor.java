7cd3fe54af5b04aca6cca85a7aa27983b31fb63f
BasketBandit <errorsfound@hotmail.co.jp>
2019-03-12 12:11:59 +0000

    verbosified the command executor to make it easier to read and also maintain in the future

--- src/main/java/com/yuuko/core/CommandExecutor.java
+++ src/main/java/com/yuuko/core/CommandExecutor.java
@@ -1,5 +1,6 @@
 package com.yuuko.core;
 
+import com.yuuko.core.commands.Command;
 import com.yuuko.core.commands.Module;
 import com.yuuko.core.database.BindFunctions;
 import com.yuuko.core.database.GuildFunctions;
@@ -15,53 +16,75 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+
 public class CommandExecutor {
 
     private static final Logger log = LoggerFactory.getLogger(CommandExecutor.class);
 
+    private static final String[] disconnectedCommands = new String[]{"play", "search", "background"};
+    private static final String[] nonDJModeCommands = new String[]{"queue", "current", "last"};
+
     public CommandExecutor(MessageReceivedEvent e, Module module, String[] cmd) {
-        if(e == null) { // Is the event null?
+        Command command = null;
+
+        // Is the event null?
+        if(e == null) {
             return;
         }
 
-        if(module.isEnabled(e)) { // Is the module enabled?
-            if(checkBinding(e, module, cmd)) { // Does the command pass the binding checks?
-                if(module.getName().equals("Audio") && !checkAudio(e, cmd)) { // Is module named Audio? If so, does the user fail any of the checks?
-                    return;
-                }
-                module.getCommandsAsList().stream().filter(command -> command.getName().equalsIgnoreCase(cmd[0])).findFirst().ifPresent(command -> {
-                    if(command.isEnabled(e)) { // Is the command enabled on the server?
-                        if((module.isNSFW() && ! Utils.isChannelNSFW(e)) || (command.isNSFW() && ! Utils.isChannelNSFW(e))) { // Is the module or command NSFW? If so is the channel /NOT/ NSFW?
-                            EmbedBuilder embed = new EmbedBuilder().setTitle("Invalid Channel").setDescription("That command can only be used in NSFW marked channels.");
-                            MessageHandler.sendMessage(e, embed.build());
-                        } else {
-                            if(command.getPermissions() != null && ! e.getGuild().getMemberById(Configuration.BOT_ID).hasPermission(command.getPermissions())) { // Is the command permission /NOT/ NULL? If so, does the bot have the permission?
-                                EmbedBuilder embed = new EmbedBuilder().setTitle("Missing Permission").setDescription("I require the '**" + Utils.getCommandPermissions(command.getPermissions()) + "**' permissions to use that command.");
-                                MessageHandler.sendMessage(e, embed.build());
-                            } else {
-                                if(command.getPermissions() != null && ! e.getMember().hasPermission(command.getPermissions()) && ! e.getMember().hasPermission(e.getTextChannel(), command.getPermissions())) { // Is the command permission NULL? If so, does the user have the permission?
-                                    EmbedBuilder embed = new EmbedBuilder().setTitle("Missing Permission").setDescription("You require the '**" + Utils.getCommandPermissions(command.getPermissions()) + "**' permissions to use that command.");
-                                    MessageHandler.sendMessage(e, embed.build());
-                                } else {
-                                    if(Sanitiser.checkParameters(e, cmd, command.getExpectedParameters(), true)) { // Does the command contain the minimum number of parameters?
-                                        try {
-                                            log.trace("Invoking {}#onCommand()", command.getClass().getName());
-                                            command.onCommand(e, cmd);
-                                            MetricsManager.getEventMetrics().COMMANDS_EXECUTED.getAndIncrement();
-                                        } catch(Exception ex) {
-                                            log.error("An error occurred while running the {} class, message: {}", command.getClass().getSimpleName(), ex.getMessage(), ex);
-                                            e.getMessage().addReaction("❌").queue();
-                                            MetricsManager.getEventMetrics().COMMANDS_FAILED.getAndIncrement();
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                });
+        for(Command comm: module.getCommandsAsList()) {
+            if(comm.getName().equalsIgnoreCase(cmd[0])) {
+                command = comm;
+                break;
             }
         }
-        messageCleanup(e);
+
+        // Is the module enabled and does the command pass the binding checks?
+        // Is module named "audio" and if so, does the user fail any of the checks?
+        if(command == null || !command.isEnabled(e) || !module.isEnabled(e) || !checkBinding(e, module, cmd) || (module.getName().equals("Audio") && !checkAudio(e, cmd))) {
+            return;
+        }
+
+        // Is the command or module NSFW? If they are, is the channel they're being used in /not/ NSFW?
+        if((command.isNSFW() || module.isNSFW()) && !Utils.isChannelNSFW(e)) {
+            EmbedBuilder embed = new EmbedBuilder().setTitle("Invalid Channel").setDescription("That command can only be used in NSFW marked channels.");
+            MessageHandler.sendMessage(e, embed.build());
+            return;
+        }
+
+        // Does the command have any permissions?
+        if(command.getPermissions() != null) {
+            // Does the bot have the permission?
+            if(!e.getGuild().getMemberById(Configuration.BOT_ID).hasPermission(command.getPermissions())) {
+                EmbedBuilder embed = new EmbedBuilder().setTitle("Missing Permission").setDescription("I require the '**" + Utils.getCommandPermissions(command.getPermissions()) + "**' permissions to use that command.");
+                MessageHandler.sendMessage(e, embed.build());
+                return;
+            }
+
+            // Does the user have the permission?
+            if(!e.getMember().hasPermission(command.getPermissions()) && !e.getMember().hasPermission(e.getTextChannel(), command.getPermissions())) {
+                EmbedBuilder embed = new EmbedBuilder().setTitle("Missing Permission").setDescription("You require the '**" + Utils.getCommandPermissions(command.getPermissions()) + "**' permissions to use that command.");
+                MessageHandler.sendMessage(e, embed.build());
+                return;
+            }
+        }
+
+        // Does the command contain the minimum number of parameters?
+        if(!Sanitiser.checkParameters(e, cmd, command.getExpectedParameters(), true)) {
+            return;
+        }
+
+        try {
+            log.trace("Invoking {}#onCommand()", command.getClass().getName());
+            command.onCommand(e, cmd);
+            messageCleanup(e);
+            MetricsManager.getEventMetrics().COMMANDS_EXECUTED.getAndIncrement();
+        } catch(Exception ex) {
+            log.error("An error occurred while running the {} class, message: {}", command.getClass().getSimpleName(), ex.getMessage(), ex);
+            e.getMessage().addReaction("❌").queue();
+            MetricsManager.getEventMetrics().COMMANDS_FAILED.getAndIncrement();
+        }
     }
 
     /**
@@ -70,13 +93,17 @@ public class CommandExecutor {
      * @param e MessageReceivedEvent
      */
     private void messageCleanup(MessageReceivedEvent e) {
-        if(TextUtility.convertToBoolean(GuildFunctions.getGuildSetting("deleteExecuted", e.getGuild().getId()))) { // Does the server want the command message removed?
-            if(!e.getGuild().getMemberById(420682957007880223L).hasPermission(Permission.MESSAGE_MANAGE)) { // Can the bot manage messages?
-                EmbedBuilder embed = new EmbedBuilder().setTitle("Missing Permission").setDescription("I am missing the '**MESSAGE_MANAGE**' permission required to execute the 'deleteExecuted' setting. If this setting is active by mistake, use **'@Yuuko settings deleteExecuted false'** to turn it off.");
-                MessageHandler.sendMessage(e, embed.build());
-            } else {
-                e.getMessage().delete().queue();
-            }
+        // Does the server want the command message /not/ removed?
+        if(!TextUtility.convertToBoolean(GuildFunctions.getGuildSetting("deleteExecuted", e.getGuild().getId()))) {
+            return;
+        }
+
+        // Does the bot have permission to remove the message?
+        if(e.getGuild().getMemberById(420682957007880223L).hasPermission(Permission.MESSAGE_MANAGE)) { // Can the bot manage messages?
+            e.getMessage().delete().queue();
+        } else {
+            EmbedBuilder embed = new EmbedBuilder().setTitle("Missing Permission").setDescription("I am missing the '**MESSAGE_MANAGE**' permission required to execute the 'deleteExecuted' setting. If this setting is active by mistake, use `@Yuuko#2525 setting deleteExecuted false`.");
+            MessageHandler.sendMessage(e, embed.build());
         }
     }
 
@@ -88,28 +115,36 @@ public class CommandExecutor {
      * @return boolean
      */
     private boolean checkAudio(MessageReceivedEvent e, String[] command) {
+        // Is the member /not/ in a voice channel?
         if(!e.getMember().getVoiceState().inVoiceChannel()) {
             EmbedBuilder embed = new EmbedBuilder().setTitle("This command can only be used while in a voice channel.");
             MessageHandler.sendMessage(e, embed.build());
             return false;
         }
 
-        if(Configuration.LAVALINK.getLavalink().getLink(e.getGuild()).getState() == Link.State.NOT_CONNECTED && !command[0].equals("play") && !command[0].equals("search") && !command[0].equals("background")) {
+        // Is Lavalink /not/ connected and does the command require it to be?
+        if(Configuration.LAVALINK.getLavalink().getLink(e.getGuild()).getState() == Link.State.NOT_CONNECTED && !Arrays.asList(disconnectedCommands).contains(command[0])) {
             EmbedBuilder embed = new EmbedBuilder().setTitle("There is no active audio connection.");
             MessageHandler.sendMessage(e, embed.build());
+
             return false;
         }
 
-        if(!e.getMember().getPermissions().contains(Permission.ADMINISTRATOR)) {
-            if(TextUtility.convertToBoolean(GuildFunctions.getGuildSetting("djMode", e.getGuild().getId()))) {
-                if(e.getMember().getRoles().stream().noneMatch(role -> role.getName().equals("DJ"))) {
-                    if(!command[0].equals("queue") && !command[0].equals("current") && !command[0].equals("last")) {
-                        EmbedBuilder embed = new EmbedBuilder().setTitle("DJ Mode Enabled").setDescription("While DJ mode is active, only a user with the role of 'DJ' can use that command.");
-                        MessageHandler.sendMessage(e, embed.build());
-                        return false;
-                    }
-                }
-            }
+        // Is the member an administrator?
+        if(e.getMember().getPermissions().contains(Permission.ADMINISTRATOR)) {
+            return true;
+        }
+
+        // Is DJ mode /not/ on?
+        if(!TextUtility.convertToBoolean(GuildFunctions.getGuildSetting("djMode", e.getGuild().getId()))) {
+            return true;
+        }
+
+        // Does the member /not/ have the DJ role and if not, is the command a DJ mode command?
+        if(e.getMember().getRoles().stream().noneMatch(role -> role.getName().equals("DJ")) && !Arrays.asList(nonDJModeCommands).contains(command[0])) {
+            EmbedBuilder embed = new EmbedBuilder().setTitle("DJ Mode Enabled").setDescription("While DJ mode is active, only a user with the role of 'DJ' can use that command.");
+            MessageHandler.sendMessage(e, embed.build());
+            return false;
         }
 
         return true;
@@ -124,18 +159,17 @@ public class CommandExecutor {
      */
     private boolean checkBinding(MessageReceivedEvent e, Module module, String[] command) {
         try {
+            // Does the bind check pass?
             if(BindFunctions.checkBind(e.getGuild().getId(), e.getChannel().getId(), module.getName())) {
                 return true;
+            } else {
+                EmbedBuilder embed = new EmbedBuilder().setTitle("Module Bound").setDescription("The **" + command[0] + "** command is bound to **" + BindFunctions.getBindsByModule(e.getGuild(), module.getName(), ", ") + "**.");
+                MessageHandler.sendMessage(e, embed.build());
+                return false;
             }
-
-            EmbedBuilder embed = new EmbedBuilder().setTitle("Module Bound").setDescription("The **" + command[0] + "** command is bound to **" + BindFunctions.getBindsByModule(e.getGuild(), module.getName(), ", ") + "**.");
-            MessageHandler.sendMessage(e, embed.build());
-
-            return false;
-
         } catch(Exception ex) {
             log.error("An error occurred while running the {} class, message: {}", command.getClass().getSimpleName(), ex.getMessage(), ex);
-            return true;
+            return true; // Would rather allow commands everywhere than nowhere if bindings broke.
         }
     }
 
