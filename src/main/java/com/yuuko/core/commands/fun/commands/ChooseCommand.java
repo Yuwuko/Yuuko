bf96ab4e9df2289acd1a69f3890ee7c7dbcc2dd3
BasketBandit <errorsfound@hotmail.co.jp>
2019-03-29 20:26:55 +0000

    updated choose command to better represent available choices, what was chosen and the probability of that choice

--- src/main/java/com/yuuko/core/commands/fun/commands/ChooseCommand.java
+++ src/main/java/com/yuuko/core/commands/fun/commands/ChooseCommand.java
@@ -7,7 +7,10 @@ import com.yuuko.core.commands.fun.FunModule;
 import com.yuuko.core.events.extensions.MessageEvent;
 import net.dv8tion.jda.core.EmbedBuilder;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.Instant;
+import java.util.Arrays;
 import java.util.Random;
 
 public class ChooseCommand extends Command {
@@ -21,8 +24,9 @@ public class ChooseCommand extends Command {
         String[] commandParameters = e.getCommand()[1].split("\\s*(,)\\s*");
 
         EmbedBuilder embed = new EmbedBuilder()
-                .setTitle("Choose")
-                .setDescription((commandParameters.length > 1) ? commandParameters[new Random().nextInt(commandParameters.length)] : commandParameters[0])
+                .addField("Choices (" + commandParameters.length + ")", Arrays.asList(commandParameters).toString(), false)
+                .addField("Selected", (commandParameters.length > 1) ? commandParameters[new Random().nextInt(commandParameters.length)] : commandParameters[0], true)
+                .addField("Probability", new BigDecimal(100.0/commandParameters.length).setScale(2, RoundingMode.HALF_UP) + "%", true)
                 .setTimestamp(Instant.now())
                 .setFooter(Configuration.STANDARD_STRINGS[1] + e.getMember().getEffectiveName(), e.getAuthor().getEffectiveAvatarUrl());
         MessageHandler.sendMessage(e, embed.build());
