81c3c6c097a68718fecc2b55fb6b38787cdcd686
BasketBandit <errorsfound@hotmail.co.jp>
2020-11-20 22:19:41 +0000

    fixed ReactionRoleCommand throwing exception when message missing

--- src/main/java/com/yuuko/core/commands/utility/commands/ReactionRoleCommand.java
+++ src/main/java/com/yuuko/core/commands/utility/commands/ReactionRoleCommand.java
@@ -8,7 +8,6 @@ import com.yuuko.core.events.entity.MessageEvent;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Emote;
-import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.message.guild.react.GenericGuildMessageReactionEvent;
 import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionAddEvent;
@@ -23,8 +22,10 @@ public class ReactionRoleCommand extends Command {
 
     @Override
     public void onCommand(MessageEvent e) {
-        String[] parameters = e.getParameters().split("\\s+");
-        Role highestSelfRole = e.getGuild().getSelfMember().getRoles().get(0); // Role list is ordered from highest to lowest
+        final String[] parameters = e.getParameters().split("\\s+");
+        final Role highestSelfRole = e.getGuild().getSelfMember().getRoles().get(0); // Role list is ordered from highest to lowest
+        final Role role = (e.getMessage().getMentionedRoles().size() > 0) ? e.getMessage().getMentionedRoles().get(0) : null;
+        final Emote emote = (e.getMessage().getEmotes().size() > 0) ? e.getMessage().getEmotes().get(0) : null;
 
         if(parameters[0].length() < 18) {
             EmbedBuilder embed = new EmbedBuilder().setTitle("Missing Message").setDescription("Input did not match any known message id.");
@@ -37,58 +38,61 @@ public class ReactionRoleCommand extends Command {
             ReactionRoleFunctions.removeReactionRole(e.getMessage().getId());
         }
 
-        final Message message = e.getChannel().retrieveMessageById(parameters[0]).complete();
-        final Role role = (e.getMessage().getMentionedRoles().size() > 0) ? e.getMessage().getMentionedRoles().get(0) : null;
-        final Emote emote = (e.getMessage().getEmotes().size() > 0) ? e.getMessage().getEmotes().get(0) : null;
-
-        if(message == null || emote == null) {
+        if(emote == null) {
             // error
             return;
         }
 
-        if(role == null) {
-            message.getReactions().stream().filter(m -> m.getReactionEmote().getEmote().equals(emote)).forEach(reaction -> {
-                reaction.removeReaction().queue(
-                        s -> {
-                            ReactionRoleFunctions.removeReactionRole(message, emote);
-                            EmbedBuilder embed = new EmbedBuilder().setTitle("Success").setDescription("Successfully removed reaction role " + emote.getAsMention() + " from message " + message + ".");
-                            MessageHandler.sendMessage(e, embed.build());
-                        },
-                        f -> {
-                            EmbedBuilder embed = new EmbedBuilder().setTitle("Failure").setDescription("Unable to remove the reaction from the selected message.");
-                            MessageHandler.sendMessage(e, embed.build());
-                        });
-            });
-            return;
-        }
-
-        // check if the role exists, is available for use.
-        if(!e.getGuild().getRoleCache().asList().contains(role)) {
-            EmbedBuilder embed = new EmbedBuilder().setTitle("Invalid Role").setDescription("This role is unavailable for use in a reaction role. Make sure that you are using roles from this server.");
-            MessageHandler.sendMessage(e, embed.build());
-            return;
-        }
-
-        // checks if role is lower in the hierarchy than bots highest role.
-        if(role.getPositionRaw() >= highestSelfRole.getPositionRaw()) {
-            EmbedBuilder embed = new EmbedBuilder().setTitle("Invalid Role").setDescription("I cannot assign roles that are higher than or equal to my highest role in the hierarchy.");
-            MessageHandler.sendMessage(e, embed.build());
-            return;
-        }
-
-        message.addReaction(emote).queue(
-                s -> {
-                    if(ReactionRoleFunctions.addReactionRole(e.getGuild(), message.getId(), emote, role)) {
-                        EmbedBuilder embed = new EmbedBuilder().setTitle("Success").setDescription("Successfully paired emote " + emote.getAsMention() + " to role " + role.getAsMention() + " for message " + message + ".");
-                        MessageHandler.sendMessage(e, embed.build());
-                    } else {
-                        EmbedBuilder embed = new EmbedBuilder().setTitle("Already Exists").setDescription("A reaction role using this message and emote combination already exists.");
-                        MessageHandler.sendMessage(e, embed.build());
-                    }},
-                f -> {
-                    EmbedBuilder embed = new EmbedBuilder().setTitle("Failure").setDescription("Unable to add a reaction to the selected message.");
-                    MessageHandler.sendMessage(e, embed.build());
+        // uses consumer .queue() instead of .complete() since .complete() will throw an exception rather than return null :)))
+        e.getChannel().retrieveMessageById(parameters[0]).queue(message -> {
+            if(role == null) {
+                message.getReactions().stream().filter(m -> m.getReactionEmote().getEmote().equals(emote)).forEach(reaction -> {
+                    reaction.removeReaction().queue(
+                            s -> {
+                                ReactionRoleFunctions.removeReactionRole(message, emote);
+                                EmbedBuilder embed = new EmbedBuilder().setTitle("Success").setDescription("Successfully removed reaction role " + emote.getAsMention() + " from message " + message + ".");
+                                MessageHandler.sendMessage(e, embed.build());
+                            },
+                            f -> {
+                                EmbedBuilder embed = new EmbedBuilder().setTitle("Failure").setDescription("Unable to remove the reaction from the selected message.");
+                                MessageHandler.sendMessage(e, embed.build());
+                            });
                 });
+                return;
+            }
+
+            // check if the role exists, is available for use.
+            if(!e.getGuild().getRoleCache().asList().contains(role)) {
+                EmbedBuilder embed = new EmbedBuilder().setTitle("Invalid Role").setDescription("This role is unavailable for use in a reaction role. Make sure that you are using roles from this server.");
+                MessageHandler.sendMessage(e, embed.build());
+                return;
+            }
+
+            // checks if role is lower in the hierarchy than bots highest role.
+            if(role.getPositionRaw() >= highestSelfRole.getPositionRaw()) {
+                EmbedBuilder embed = new EmbedBuilder().setTitle("Invalid Role").setDescription("I cannot assign roles that are higher than or equal to my highest role in the hierarchy.");
+                MessageHandler.sendMessage(e, embed.build());
+                return;
+            }
+
+            message.addReaction(emote).queue(
+                    s -> {
+                        if(ReactionRoleFunctions.addReactionRole(e.getGuild(), message.getId(), emote, role)) {
+                            EmbedBuilder embed = new EmbedBuilder().setTitle("Success").setDescription("Successfully paired emote " + emote.getAsMention() + " to role " + role.getAsMention() + " for message " + message + ".");
+                            MessageHandler.sendMessage(e, embed.build());
+                        } else {
+                            EmbedBuilder embed = new EmbedBuilder().setTitle("Already Exists").setDescription("A reaction role using this message and emote combination already exists.");
+                            MessageHandler.sendMessage(e, embed.build());
+                        }},
+                    f -> {
+                        EmbedBuilder embed = new EmbedBuilder().setTitle("Failure").setDescription("Unable to add a reaction to the selected message.");
+                        MessageHandler.sendMessage(e, embed.build());
+                    });
+
+        }, failure -> {
+            EmbedBuilder embed = new EmbedBuilder().setTitle("Failure").setDescription("Unable to find message with given id, either deleted or isn't in this channel.");
+            MessageHandler.sendMessage(e, embed.build());
+        });
     }
 
     /**
