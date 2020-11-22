868f256e16a0b466e3de4e87ce9b492a6d0fa465
BasketBandit <errorsfound@hotmail.co.jp>
2020-11-22 16:20:03 +0000

    added shutdown task to periodically check for shutdown signal

---
+++ src/main/java/com/yuuko/core/scheduler/tasks/ShutdownTask.java
@@ -0,0 +1,19 @@
+package com.yuuko.core.scheduler.tasks;
+
+import com.yuuko.core.Config;
+import com.yuuko.core.database.function.DatabaseFunctions;
+import com.yuuko.core.scheduler.Task;
+
+public class ShutdownTask implements Task {
+    @Override
+    public void run() {
+        Config.SHARD_MANAGER.getShards().forEach(shard -> {
+            if(DatabaseFunctions.hasShutdownSignal(shard.getShardInfo().getShardId())) {
+                log.warn("Shutdown signal detected for shard {}, shutting down...", shard.getShardInfo().getShardId());
+                shard.shutdown();
+                DatabaseFunctions.cancelShutdownSignal(shard.getShardInfo().getShardId());
+                DatabaseFunctions.updateShardStatistics(shard);
+            }
+        });
+    }
+}
