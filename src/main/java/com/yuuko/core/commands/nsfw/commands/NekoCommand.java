c0cd935fccf3674a10f3fb6f8edd46f77d9615d7
BasketBandit <errorsfound@hotmail.co.jp>
2021-01-21 18:16:58 +0000

    improve NekoCommand parameter handling

--- src/main/java/com/yuuko/core/commands/nsfw/commands/NekoCommand.java
+++ src/main/java/com/yuuko/core/commands/nsfw/commands/NekoCommand.java
@@ -6,13 +6,14 @@ import com.yuuko.core.Yuuko;
 import com.yuuko.core.commands.Command;
 import com.yuuko.core.events.entity.MessageEvent;
 import com.yuuko.core.io.RequestHandler;
-import com.yuuko.core.utilities.Sanitiser;
 import net.dv8tion.jda.api.EmbedBuilder;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class NekoCommand extends Command {
     private static final String BASE_URL = "https://nekos.life/api/v2/img/";
+    private static final List<String> ENDPOINTS = Arrays.asList("femdom", "nekoapi_v3", "tickle", "classic", "ngif", "erofeet", "meow", "erok", "poke", "les", "hololewd", "lewdk", "keta", "feetg", "nsfw_neko_gif", "eroyuri", "kiss", "8ball", "kuni", "tits", "pussy_jpg", "cum_jpg", "pussy", "lewdkemo", "lizard", "slap", "lewd", "cum", "cuddle", "spank", "smallboobs", "Random_hentai_gif", "avatar", "fox_girl", "nsfw_avatar", "hug", "gecg", "boobs", "pat", "feet", "smug", "kemonomimi", "solog", "holo", "wallpaper", "bj", "woof", "yuri", "trap", "anal", "baka", "blowjob", "holoero", "feed", "neko", "gasm", "hentai", "futanari", "ero", "solo", "waifu", "pwankg", "eron", "erokemo");
 
     public NekoCommand() {
         super("neko", Yuuko.MODULES.get("nsfw"), 0, -1L, Arrays.asList("-neko", "-neko <type>"), true, null);
@@ -20,7 +21,7 @@ public class NekoCommand extends Command {
 
     @Override
     public void onCommand(MessageEvent e) throws Exception {
-        final String url = BASE_URL + ((e.hasParameters()) ? Sanitiser.scrub(e.getParameters(), true) : "lewd");
+        final String url = BASE_URL + ((e.hasParameters() && ENDPOINTS.contains(e.getParameters().toLowerCase())) ? e.getParameters().toLowerCase() : "lewd");
 
         final JsonObject json = new RequestHandler(url).getJsonObject();
         if(json != null && !json.has("msg")) {
@@ -32,7 +33,7 @@ public class NekoCommand extends Command {
             return;
         }
 
-        EmbedBuilder embed = new EmbedBuilder().setTitle("Invalid Parameter").setDescription("'femdom', 'nekoapi_v3', 'tickle', 'classic', 'ngif', 'erofeet', 'meow', 'erok', 'poke', 'les', 'hololewd', 'lewdk', 'keta', 'feetg', 'nsfw_neko_gif', 'eroyuri', 'kiss', '8ball', 'kuni', 'tits', 'pussy_jpg', 'cum_jpg', 'pussy', 'lewdkemo', 'lizard', 'slap', 'lewd', 'cum', 'cuddle', 'spank', 'smallboobs', 'Random_hentai_gif', 'avatar', 'fox_girl', 'nsfw_avatar', 'hug', 'gecg', 'boobs', 'pat', 'feet', 'smug', 'kemonomimi', 'solog', 'holo', 'wallpaper', 'bj', 'woof', 'yuri', 'trap', 'anal', 'baka', 'blowjob', 'holoero', 'feed', 'neko', 'gasm', 'hentai', 'futanari', 'ero', 'solo', 'waifu', 'pwankg', 'eron', 'erokemo'");
+        EmbedBuilder embed = new EmbedBuilder().setTitle("Invalid Parameter").setDescription(Arrays.toString(ENDPOINTS.toArray()));
         MessageDispatcher.reply(e, embed.build());
     }
 
