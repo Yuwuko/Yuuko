b3ef05fe7329578056de31d25434d67d9f40a2fd
BasketBandit <errorsfound@hotmail.co.jp>
2021-03-04 09:38:44 +0000

    fixed bug with SettingsCommand output when database entries aren't properly removed/updated

--- src/main/java/com/yuuko/core/commands/setting/commands/SettingsCommand.java
+++ src/main/java/com/yuuko/core/commands/setting/commands/SettingsCommand.java
@@ -6,6 +6,7 @@ import com.yuuko.core.commands.Command;
 import com.yuuko.core.database.function.GuildFunctions;
 import com.yuuko.core.events.entity.MessageEvent;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.TextChannel;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,6 +21,31 @@ public class SettingsCommand extends Command {
     public void onCommand(MessageEvent e) throws Exception {
         // Embed displaying all of the current settings for the server, giving information about each setting.
         ArrayList<String> settingsList = GuildFunctions.getGuildSettings(e.getGuild().getId());
+
+        String starboard = "Disabled";
+        if(settingsList.get(4) != null) {
+            TextChannel textChannel = e.getGuild().getTextChannelById(settingsList.get(4));
+            starboard = textChannel != null ? textChannel.getAsMention() : "Unable to find starboard, please reset.";
+        }
+
+        String commandlog = "Disabled";
+        if(settingsList.get(5) != null) {
+            TextChannel textChannel = e.getGuild().getTextChannelById(settingsList.get(5));
+            commandlog = textChannel != null ? textChannel.getAsMention() : "Unable to find commandlog, please reset.";
+        }
+
+        String moderationlog = "Disabled";
+        if(settingsList.get(6) != null) {
+            TextChannel textChannel = e.getGuild().getTextChannelById(settingsList.get(6));
+            moderationlog = textChannel != null ? textChannel.getAsMention() : "Unable to find moderationlog, please reset.";
+        }
+
+        String eventchannel = "Disabled";
+        if(settingsList.get(7) != null) {
+            TextChannel textChannel = e.getGuild().getTextChannelById(settingsList.get(7));
+            eventchannel = textChannel != null ? textChannel.getAsMention() : "Unable to find eventchannel, please reset.";
+        }
+
         EmbedBuilder embed = new EmbedBuilder()
                 .setTitle("Settings for `" + e.getGuild().getName() + "`")
                 .setDescription("Settings can be changed by typing one of the commands listed below using the specified data type, e.g. `<boolean>` which you would replace with either `true` or `false`.")
@@ -27,10 +53,10 @@ public class SettingsCommand extends Command {
                 .addField(e.getPrefix() + "cleanupcommands <boolean>", "`" + settingsList.get(1) + "` - Deletes the users command when it is executed.", false)
                 .addField(e.getPrefix() + "playnotifications <boolean>", "`" + settingsList.get(2) + "` - Sends information of the mew track when it changes.", false)
                 .addField(e.getPrefix() + "djmode <boolean>", "`" + settingsList.get(3) + "` - Defines if DJ mode is on, meaning only users with the role 'DJ' can use certain audio commands.", false)
-                .addField(e.getPrefix() + "starboard <#channel>", (settingsList.get(4) == null ? "`Disabled`" : e.getGuild().getTextChannelById(settingsList.get(4)).getAsMention()) + " - Where any messages reacted to with a ⭐ will be sent.", false)
-                .addField(e.getPrefix() + "commandlog <#channel>", (settingsList.get(5) == null ? "`Disabled`" : e.getGuild().getTextChannelById(settingsList.get(5)).getAsMention() ) + " - Sends executed commands to a defined log channel.", false)
-                .addField(e.getPrefix() + "moderationlog <#channel>", (settingsList.get(6) == null ? "`Disabled`" :  e.getGuild().getTextChannelById(settingsList.get(6)).getAsMention()) + " - Sends moderation events to a defined log channel.", false)
-                .addField(e.getPrefix() + "eventchannel <#channel>", (settingsList.get(7) == null ? "`Disabled`" : e.getGuild().getTextChannelById(settingsList.get(7)).getAsMention()) + " - Where published events will be sent.", false)
+                .addField(e.getPrefix() + "starboard <#channel>",  starboard + " - Where any messages reacted to with a ⭐ will be sent.", false)
+                .addField(e.getPrefix() + "commandlog <#channel>", commandlog + " - Sends executed commands to a defined log channel.", false)
+                .addField(e.getPrefix() + "moderationlog <#channel>", moderationlog + " - Sends moderation events to a defined log channel.", false)
+                .addField(e.getPrefix() + "eventchannel <#channel>", eventchannel + " - Where published events will be sent.", false)
                 .setFooter(Yuuko.STANDARD_STRINGS.get(1) + e.getAuthor().getAsTag(), e.getAuthor().getEffectiveAvatarUrl());
         MessageDispatcher.reply(e, embed.build());
     }
