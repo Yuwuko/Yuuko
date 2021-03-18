f202942550796bf31694f072829384eefc7e2bdb
BasketBandit <errorsfound@hotmail.co.jp>
2021-03-18 20:05:30 +0000

    optimise PruneCooldownsTask

--- src/main/java/com/yuuko/core/scheduler/tasks/PruneCooldownsTask.java
+++ src/main/java/com/yuuko/core/scheduler/tasks/PruneCooldownsTask.java
@@ -1,12 +1,13 @@
 package com.yuuko.core.scheduler.tasks;
 
 import com.yuuko.core.Yuuko;
+import com.yuuko.core.commands.Command;
 import com.yuuko.core.scheduler.Task;
 
 public class PruneCooldownsTask implements Task {
 
     @Override
     public void run() {
-        Yuuko.COMMANDS.keySet().forEach(key -> Yuuko.COMMANDS.get(key).pruneCooldowns());
+        Yuuko.COMMANDS.values().forEach(Command::pruneCooldowns);
     }
 }
